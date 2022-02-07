68db2ee8ca2308866af84b599faf3c867c2f438b
costawski <s.costa.2235@vallauri.edu>
2022-02-07 17:54:09 +0100

    Compiti(07-02-22)

--- JavaScript/Ex05 Cinque Numeri/index.js
+++ JavaScript/Ex05 Cinque Numeri/index.js
@@ -1,14 +1,44 @@
 "use strinct"
 
-let _InTxtNum;
-let _InChkNum;
+let _inTxtNum;
+let _inChkNum;
+let nRandom = [];
+let nUtente = [];
 
 function init() {
     _inTxtNum = document.getElementsByName("txtNum");
-    _InChkNum = document.getElementsByName("chkNum");
+    _inChkNum = document.getElementsByName("chkNum");
+
+    let n = generaNumero(1, 6);
+    nRandom.push(n);
+
+    for(let i = 1; i < 5; i++) {
+        let esci = false;
+        let ferma = false;
+
+        do {
+            esci = false;
+            let j = 0;
+            n = generaNumero(1, 6);
+
+            while(!esci && j < i) {
+                if(n == nRandom[j]) {
+                    esci = true;
+                }
+                j++;
+            }
+
+            if(!esci) {
+                nRandom.push(n);
+                ferma = true;
+            }
+        }while(ferma)
+    }
+    console.log(nRandom);
 }
 
-function invia() {}
+function invia() {
+}
 
 function generaNumero(a, b) { //estremo superiore escluso
     let ris = Math.floor((b-a) * Math.random()) + a;
