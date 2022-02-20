156f562190aca304161d60822d1aaea8c7fff9fc
costawski <s.costa.2235@vallauri.edu>
2022-02-20 11:57:49 +0100

    Ex

--- JavaScript/Ex09 Resistenze Elettriche/index.js
+++ JavaScript/Ex09 Resistenze Elettriche/index.js
@@ -6,12 +6,14 @@ let _lstFattore;
 let _lstTolleranza;
 let colori = ["argento", "oro", "nero", "marrone", "rosso", "arancio", "giallo", "verde", "blu", "viola", 
             "grigio", "bianco"];
+let _txtRisultato;
 
 window.onload = function() {
     _lstCifra1 = document.getElementById("lstCifra1");
     _lstCifra2 = document.getElementById("lstCifra2");
     _lstFattore = document.getElementById("lstFattore");
     _lstTolleranza = document.getElementById("lstTolleranza");
+    _txtRisultato = document.getElementById("txtRisultato");
 
     for(let i = 2; i < colori.length; i++) {
         let html = "<option value='" + (i - 2) + "'>" + colori[i] + "</option>";
@@ -25,7 +27,19 @@ window.onload = function() {
     for(let i = 0; i < colori.length - 2; i++) {
         let html = `<option value='${(i - 2)}'> ${colori[i]} </option>`;
         _lstFattore.innerHTML += html;
+
+        if(i != 2 && i != 5 && i != 6 && i != 10 && i != 11) {
+            _lstTolleranza.innerHTML += html;
+        }
     }
 
     _lstFattore.selectedIndex = -1;
+    _lstTolleranza.selectedIndex = -1;
+}
+
+function calcola() {
+    let cifre;
+
+    cifre = colori.lastIndexOf(_lstCifra1.innerHTML.innerHTML);
+    console.log(cifre);
 }
