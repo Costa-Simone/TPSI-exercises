f16c4cd364c48345a6ab61c6822a285afb75e609
costawski <s.costa.2235@vallauri.edu>
2022-03-14 21:14:47 +0100

    Ex

--- JavaScript/Ex18 black jack grafico/index.js
+++ JavaScript/Ex18 black jack grafico/index.js
@@ -2,17 +2,14 @@
 
 let vet = []
 
-let _assoG;
-let _cartaG;
-
 window.onload=function(){
     let _btnG = document.getElementsByClassName("card")[0];
-    _cartaG = document.getElementsByClassName("card")[1];
+    let _cartaG = document.getElementsByClassName("card")[1];
     let _btnB = document.getElementsByClassName("card")[2];
     let _cartaB = document.getElementsByClassName("card")[3];
 	let _puntiG = document.getElementsByTagName("span")[0];
 	let _puntiB = document.getElementsByTagName("span")[1];	
-    _assoG = document.getElementsByClassName("msgAsso")[0];
+    let _assoG = document.getElementsByClassName("msgAsso")[0];
     let _assoB = document.getElementsByClassName("msgAsso")[1];
     let _chkG = _assoG.children[0];
     let _chkB = _assoB.children[0];
@@ -23,20 +20,21 @@ window.onload=function(){
     _btnG.addEventListener("mouseout", function() { _btnG.style.opacity = "50%"; });
     _btnG.addEventListener("click", assegnaCarta);
 
-    _btnB.src = "img/dorso.gif";
-    _btnG.src = "img/dorso.gif";
+    _btnB.style.backgroudImage = "url('img/dorso.gif')";
+    _btnG.style.backgroudImage = "url('img/dorso.gif')";
     _assoB.style.visibility = "hidden";
     _cartaB.src = "";
     _cartaG.src = "";
     _btnB.style.opacity = "50%";
     _btnG.style.opacity = "50%";
-}
 
-function assegnaCarta() {
-    let n = generaNumero(1, 14);
-    let c = generaNumero(97, 101)[0];
-
-    _cartaG.src = "img/" + c[0] + n + ".gif";
+    function assegnaCarta() {
+        let n = generaNumero(1, 14);
+        let c = String.fromCharCode(generaNumero(97, 101));
+        console.log(c);
+    
+        _cartaG.style.backgroudImage = "url('img/" + c + n + ".gif')";
+    }
 }
 
 function generaNumero(a, b){
