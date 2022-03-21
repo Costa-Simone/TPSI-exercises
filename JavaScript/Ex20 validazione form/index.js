0930f1dbc61b6b68d797999e55d3d93767b89341
costawski <s.costa.2235@vallauri.edu>
2022-03-21 12:50:46 +0100

    Lezione(21-02-22)

--- JavaScript/Ex20 validazione form/index.js
+++ JavaScript/Ex20 validazione form/index.js
@@ -10,13 +10,24 @@ window.onload = function () {
 	let _chkLavoratore = document.getElementById("lavoratore");
     let _txtDescrizione = document.getElementById("descrizione");
 		
-	_txtMatricola.addEventListener("change", controllaMatricola)
-	_txtCognome.addEventListener("change", controllaCognome)
-	_txtNome.addEventListener("change", controllaNome)
-	_lstRegione.addEventListener("change", controllaRegione)
-	_chkLavoratore.addEventListener("click", abilitaDescrizione)
+	_txtMatricola.addEventListener("change", controllaMatricola);
+	_txtCognome.addEventListener("change", controllaCognome);
+	_txtNome.addEventListener("change", controllaNome);
+	_lstRegione.addEventListener("change", controllaRegione);
+	_chkLavoratore.addEventListener("click", abilitaDescrizione);
 	
-	document.querySelector("input[type=button]").addEventListener("click", validaForm)
-}
-	
+	document.querySelector("input[type=button]").addEventListener("click", validaForm);
 
+	function controllaMatricola() {
+		if(_txtMatricola.value.length != 12 || isDigit(_txtMatricola.value) == false) {
+			_txtMatricola.classList.add("red-border");
+		}
+		else {
+			_txtMatricola.classList.remove("red-border");
+		}
+	}
+
+	function controllaCognome() {
+		if(_txtMatricola.value.length == "" || )
+	}
+}
--- JavaScript/Ex20 validazione form/libreria.js
+++ JavaScript/Ex20 validazione form/libreria.js
@@ -18,4 +18,3 @@ function isLetter(s){
     }
 	return true;      
 }
-
