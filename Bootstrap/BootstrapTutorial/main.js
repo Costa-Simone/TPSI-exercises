acc4f2a63de7fab77b553a4d28a19e877acb4c28
costawski <s.costa.2235@vallauri.edu>
2022-04-26 13:16:49 +0200

    Lezione(26-04-22)

--- Bootstrap/BootstrapTutorial/main.js
+++ Bootstrap/BootstrapTutorial/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function () {
-    $('.header').height($(window).height());
+// resize header to size of browser window
+
+var ready = (callback) => {
+	if (document.readyState != "loading") callback();
+	else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => { 
+	document.querySelector(".header").style.height = window.innerHeight + "px";
 })
+
+// set modal time delay before loading
+
+setTimeout(function() {
+	$('#demo-modal').modal();
+}, 500);
