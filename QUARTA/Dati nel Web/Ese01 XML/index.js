f54e945255d768658db348a82bce23e978d061ed
Costa Simone <s.costa.2235@vallauri.edu>
2022-09-13 11:55:13 +0200

    Organizzazione

---
+++ QUARTA/Dati nel Web/Ese01 XML/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function crea() {
+    localStorage.setItem("bookstore_xml", bookstore);
+    alert("Dati salvati correttamente nel localStorage")
+}
+
+function visualizza() {
+    // lettura della stringa dal localStorage
+    let xml = localStorage.getItem("bookstore_xml");
+
+
+
+
+}
