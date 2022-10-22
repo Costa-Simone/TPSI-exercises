28c30be7ba2fd6a40e15ac00bc6a322972ab0ada
Costa Simone <s.costa.2235@vallauri.edu>
2022-10-22 08:26:35 +0200

    Upgrade

--- QUARTA/JQuery/Ex03 visualizzatore Immagini/index.js
+++ QUARTA/JQuery/Ex03 visualizzatore Immagini/index.js
@@ -1,5 +1,47 @@
 "use strict"
 
 $(document).ready(function() {
+  let _btnIndietro = $("#btnIndietro")
+  let _btnAvanti = $("#btnAvanti")
+  let _img = $("#img")
+  let _wrapper = $("#wrapper")
+  _wrapper.css("text-align","center")
+  
+  let i = 1
+  let cssPulsanti = {
+    "width": "140px",
+    "height": "40px",
+    "font-weight" : "bold",
+    "border-radius":"50%",
+    "background-color":"orange",
+  }
+  _btnIndietro.css(cssPulsanti)
+  _btnAvanti.css(cssPulsanti)
+  _img.css("width",400)
+  _img.css("text-align","center")
+  _img.css("vertical-align","middle")
+  _btnIndietro.prop("disabled",true)
+  _img.prop("src","img/img1.jpg")
+  _btnAvanti.on("click",function(){
+      if(i<=7){
+        i++
+      }
+      _img.prop("src","img/img"+i+".jpg")
+      if(i==7){
+        _btnAvanti.prop("disabled",true)
+        _btnIndietro.prop("disabled",false)
+      }
+  })
+
+  _btnIndietro.on("click",function(){
+    if(i>=1){
+      i--
+    }
+    _img.prop("src","img/img"+i+".jpg")
+    if(i==1){
+      _btnAvanti.prop("disabled",false)
+      _btnIndietro.prop("disabled",true)
+    }
+  })
    
 });
--- QUARTA/JQuery/Ex04 Animate/index.js
+++ QUARTA/JQuery/Ex04 Animate/index.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
 
     var _btnAvvia = $("#btnAvvia");
-	_btnAvvia.on("click", eseguiAnimazione);
+	_btnAvvia.on("click", eseguiAnimazione)
+
+	lampeggio()
+	setInterval(lampeggio, 900)
  
 	function eseguiAnimazione(){ 
+		_btnAvvia.off("click")
+
 		$("#pedina")		
 		.css({"left":"10px","top":"260px", "width":"15px", "height":"15px"})
 		.animate({"left":'+=60px', "width":"8px",  "height":"8px"}, 1300)
 		.animate({"top":'+=38px',  "width":"15px", "height":"15px"},1300)
 		.animate({"left":'+=116px',"width":"8px",  "height":"8px"}, 1300)
 		.animate({"top":'+=77px',  "width":"15px", "height":"15px"},1300)
-		.animate({"left":'+=250px',"width":"8px",  "height":"8px"}, 1300)
+		.animate({"left":'+=250px',"width":"8px",  "height":"8px"}, 1300, function() {
+			_btnAvvia.on("click", eseguiAnimazione)
+		})
 	}
-	
-});
+
+	function lampeggio() {
+		_btnAvvia.animate({"opacity" : 0}, 450, function() {
+			_btnAvvia.animate({"opacity" : 1}, 450, lampeggio)
+		})
+	}
+})
